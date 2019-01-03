cac1329380b0f983ba734e660a1e4448cf82ea39
汪峰 <wangfeng@jeeshare.com>
2019-01-03 17:57:57 +0800

    gson类提交

---
+++ app/src/main/java/com/weather/app/weather/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.weather.app.weather.gson;
+
+public class AQI {
+
+    public AQICity aqiCity;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
